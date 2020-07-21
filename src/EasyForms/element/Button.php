f7c4e7c11b7613ddf3ade7c22fa51861de629d2c
GiantQuartz <giantquartz@gmail.com>
2020-07-21 22:01:55 +0200

    add open source license

--- src/EasyForms/element/Button.php
+++ src/EasyForms/element/Button.php
@@ -1,8 +1,9 @@
 <?php
-/*
- * Copyright (C) GiantQuartz - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  */
 
 declare(strict_types=1);
